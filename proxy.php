7ad435e3556a861213e70c590e496a0b9d93bc93
Jia Xun <chaijiaxun.gmail.com>
2016-09-06 15:58:24 -0600

    Added comments

--- proxy.php
+++ proxy.php
@@ -1,5 +1,10 @@
 <?php
+/**
+ * File: proxy.php
+ * This file exists to allow the server to communicate cross site to the jisho api site.
+ * Javascript is unable to send a direct request due to chrome's security policies.
+ * Somehow this workaround works. So eh.
+ */
 header('Content-Type: text/html; charset=utf-8');
 echo file_get_contents("http://jisho.org/api/v1/search/words?keyword=".$_GET['keyword']);
-
 ?>
